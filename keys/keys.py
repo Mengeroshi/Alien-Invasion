068952624c8f1c097de5f696ae09f3f45461de6d
Xavier Bawerk <dpllgr@hotmail.com>
2019-08-22 00:43:22 -0500

    key printer made

---
+++ keys/keys.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+from settings import Settings
+class Launcher:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Keys IDK")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key != pygame.K_q:
+                        print(event.key)
+                    else:
+                        sys.exit()
+                        
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    rckt = Launcher()
+    rckt.run_game()
