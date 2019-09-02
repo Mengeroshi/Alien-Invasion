066a20941457da6a0986f9dc0f157f8aa6921ac4
Xavier Bawerk <dpllgr@hotmail.com>
2019-09-01 23:45:54 -0500

    Making The Fleet Move

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -84,6 +85,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
     
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        
+        self.aliens.update()
+    
     def _create_fleet(self):
         
         alien = Alien(self)
@@ -107,6 +113,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+            
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _create_constellation(self):
         star = Star(self)
